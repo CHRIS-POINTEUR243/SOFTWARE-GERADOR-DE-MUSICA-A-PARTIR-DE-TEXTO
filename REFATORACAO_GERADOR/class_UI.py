dcfdefbaf21699e7cd090af0786e20032e403508
milho-v3rd3 <isadora.smuller@gmail.com>
2025-11-30 13:58:30 -0300

    Oitavas ajeitadas
    de -5 a +5

--- REFATORACAO_GERADOR/class_UI.py
+++ REFATORACAO_GERADOR/class_UI.py
@@ -12,7 +12,7 @@ VOLUME_DEFAULT = 127  # máximo
 BPM_DEFAULT = 120
 INSTRUMENTO_DEFAULT = 'ACOUSTIC_GRAND_PIANO'
 
-opcoes_oitava = [0,1,2,3,4,5,6,7,8]
+opcoes_oitava = [-5,-4,-3,-2,-1,0,1,2,3,4,5]
 
 class UI:
     def __init__(self):
